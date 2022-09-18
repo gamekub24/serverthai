50b176c038e2699ae5b682b3e88d2d4b56909aaa
gamekub24 <seotop4you@gmail.com>
2022-09-18 23:28:39 +0700

    333333

---
+++ _my_atom_setup.md
@@ -0,0 +1,3 @@
+In the below files I've shared my Atom config and setup. Hopefully it's useful to my fellow Atom users.
+
+Please comment below and let me know if you have any questions! :sunglasses:
---
+++ packages.bash
@@ -0,0 +1,57 @@
+### Packages
+├── an-old-hope-syntax@1.0.4
+├── atom-beautify@0.30.9
+├── atom-material-syntax@1.0.8
+├── atom-material-syntax-dark@1.0.0
+├── atom-material-ui@2.1.1
+├── autoclose-html@0.23.0
+├── autocomplete-paths@2.12.2
+├── busy-signal@1.4.3
+├── chester-atom-syntax@0.3.0
+├── city-lights-syntax@1.1.6
+├── city-lights-ui@1.3.1
+├── cobalt2-syntax@0.5.3
+├── craft-twig@0.4.0
+├── dash@1.7.1
+├── double-tag@1.1.0
+├── duotone-blue-black-syntax@0.7.3
+├── duotone-blue-dark-syntax@1.4.0
+├── duotone-dank-green-syntax@0.0.0
+├── duotone-dark-desert-syntax@0.1.3
+├── duotone-dark-earth-syntax@2.1.0
+├── duotone-dark-forest-syntax@2.1.0
+├── duotone-dark-sea-syntax@2.1.0
+├── duotone-dark-sky-syntax@2.1.0
+├── duotone-dark-space-syntax@2.1.0
+├── duotone-dark-syntax@2.1.0
+├── duotone-gray-sky@1.1.1
+├── editor-stats@0.17.0
+├── editorconfig@2.2.2
+├── emmet@2.4.3
+├── file-icons@2.1.16
+├── firewatch-syntax@1.1.1
+├── gist-it@0.9.2
+├── intentions@1.1.5
+├── language-vue@0.23.1
+├── less-than-slash@0.18.0
+├── linter@2.2.0
+├── linter-eslint@8.4.1 (disabled)
+├── linter-htmlhint@1.4.0 (disabled)
+├── linter-markdown@5.2.0 (disabled)
+├── linter-mixed-indent@1.1.1 (disabled)
+├── linter-php@1.5.1 (disabled)
+├── linter-stylelint@4.2.0 (disabled)
+├── linter-twig@0.4.2 (disabled)
+├── linter-ui-default@1.6.10
+├── minimap@4.29.7
+├── minimap-linter@2.1.3
+├── native-ui@0.25.2
+├── nucleus-dark-ui@0.12.3
+├── oceanic-next@1.0.0
+├── pigments@0.40.2
+├── predawn-ui@1.0.7
+├── prettier-atom@0.50.0
+├── project-manager@3.3.5
+├── sort-lines@0.18.0
+├── teletype@0.7.0
+└── toggle-quotes@1.1.0
